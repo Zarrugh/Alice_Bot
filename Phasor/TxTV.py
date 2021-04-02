465f49c75710e42f9456268e7da8f696084ba482
Mohamed Zaroug <mzarrugh17@gmail.com>
2021-04-03 00:28:53 +0200

    Add files via upload

---
+++ Phasor/TxTV.py
@@ -0,0 +1,36 @@
+
+import gtts
+def TXT():
+    tts = gtts.gTTS("Sorry i didn't get that, can you repeat please.")
+    tts.save("SIDGTCYRP.mp3")
+#**************************************************************************************************
+    tts = gtts.gTTS("hi there can i help you?")
+    tts.save("G1.mp3")
+    tts = gtts.gTTS("hello there can i help you?")
+    tts.save("G2.mp3")
+    tts = gtts.gTTS("i'm here can i help you?"")
+    tts.save("G3.mp3")
+    tts = gtts.gTTS("go ahead can i help you?")
+    tts.save("G4.mp3")
+#**************************************************************************************************
+    tts = gtts.gTTS("i'm doning well and you?")
+    tts.save("Q1.mp3")
+    tts = gtts.gTTS("i'm doning great i hope you too doing great")
+    tts.save("Q2.mp3")
+    tts = gtts.gTTS("i'm waiting for a question from you let me know if i can help you")
+    tts.save("Q3.mp3")
+    tts = gtts.gTTS("i'm trying to learn new things")
+    tts.save("Q4.mp3")
+#**************************************************************************************************
+    tts = gtts.gTTS("how can i help you? you can ask me to open apps or turning on some stuff...etc")
+    tts.save("HCIHY.mp3")
+#**************************************************************************************************
+    tts = gtts.gTTS("Sorry, I couldn't figure out which app to open, can you say that again?")
+    tts.save("ICFOWATO.mp3")
+    tts = gtts.gTTS("Sorry, I couldn't figure out which door to lock, can you say that again?")
+    tts.save("ICFOWDTL.mp3")
+    tts = gtts.gTTS("Sorry, I couldn't figure out which door to unlock, can you say that again?")
+    tts.save("ICFOWDTUL.mp3")
+    tts = gtts.gTTS("Sorry, I couldn't figure out which object to turn, can you say that again?")
+    tts.save("ICFOWOTT.mp3")
+    return()
